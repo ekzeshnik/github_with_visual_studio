bbd6f6d43ef3115711833bbf086ba5abb978ca05
Admin <Admin@DESKTOP-EBSH3NO>
2024-12-22 02:51:59 -0800

    Restored broken project

--- github_with_visual_studio/github_with_visual_studio.cpp
+++ github_with_visual_studio/github_with_visual_studio.cpp
@@ -1,20 +1,129 @@
-// github_with_visual_studio.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include<cctype>
+
+int calculator() {
+    long double firstNumber, secondNumber, calculationResult;
+    int choice;
+
+    std::cout << "Options: \n 1) Add\n 2) Subtract\n 3) Multiply\n 4) Divide\n Your choice: ";
+    std::cin >> choice;
+    std::cout << "Enter 2 numbers: ";
+    std::cin >> firstNumber >> secondNumber;
+
+    switch (choice) {
+    case 1:
+        calculationResult = firstNumber + secondNumber;
+        std::cout << firstNumber << " + " << secondNumber << " = " << calculationResult << std::endl;
+        break;
+
+    case 2:
+        calculationResult = firstNumber - secondNumber;
+        std::cout << firstNumber << " - " << secondNumber << " = " << calculationResult << std::endl;
+        break;
+
+    case 3:
+        calculationResult = firstNumber * secondNumber;
+        std::cout << firstNumber << " * " << secondNumber << " = " << calculationResult << std::endl;
+        break;
+
+    case 4:
+        calculationResult = firstNumber / secondNumber;
+        std::cout << firstNumber << " / " << secondNumber << " = " << calculationResult << std::endl;
+        break;
+
+    default:
+        std::cout << "There is no " << choice << " option." << std::endl;
+    }
+    return 0;
+}
+
+int vowelOrConsonant() {
+    char userLetter;
+
+    std::cout << "Enter any letter: ";
+    std::cin >> userLetter;
+
+    userLetter = std::tolower(userLetter);
+
+    switch (userLetter) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        std::cout << userLetter << " is vowel." << std::endl;
+        break;
+
+    default:
+        if (std::isalpha(userLetter)) {
+            std::cout << userLetter << " is consonant." << std::endl;
+        }
+        else {
+            std::cout << userLetter << " is not a letter." << std::endl;
+        }
+    }
+    return 0;
+}
+
+int printTriangle() {
+    int triangleHeight, i, j;
+
+    std::cout << "Enter height of your triangle: ";
+    std::cin >> triangleHeight;
+
+    for (i = 0; i <= triangleHeight; i++) {
+        for (j = 0; j <= i; j++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
+
+int printAllNumbersInRange() {
+    int beginningOfTheRange, endOfTheRange;
+
+    std::cout << "Enter range borders separated by space (ex. 3 10): ";
+    std::cin >> beginningOfTheRange >> endOfTheRange;
+
+    for (int numbersFromRange = beginningOfTheRange; numbersFromRange <= endOfTheRange; numbersFromRange++) {
+        std::cout << numbersFromRange << std::endl;
+    }
+
+    return 0;
+}
+
+int printAllNumbersInRangeWithStep() {
+    int beginningOfTheRangeWithStep, endOfTheRangeWithStep, step;
+
+    std::cout << "Enter range borders separated by space (ex. 3 10): ";
+    std::cin >> beginningOfTheRangeWithStep >> endOfTheRangeWithStep;
+    std::cout << "Now enter step: ";
+    std::cin >> step;
+
+    for (int numbersFromRangeWithStep = beginningOfTheRangeWithStep; numbersFromRangeWithStep <= endOfTheRangeWithStep; numbersFromRangeWithStep += step) {
+        std::cout << numbersFromRangeWithStep << std::endl;
+    }
+
+    return 0;
+}
+
+int printAllNumbersInRangeWithStepUsingWhile() {
+    int beginningOfTheRangeWithStepUsingWhile, endOfTheRangeWithStepUsingWhile, step;
+
+    std::cout << "Enter range borders separated by space (ex. 3 10): ";
+    std::cin >> beginningOfTheRangeWithStepUsingWhile >> endOfTheRangeWithStepUsingWhile;
+    std::cout << "Now enter step: ";
+    std::cin >> step;
+
+    while ((beginningOfTheRangeWithStepUsingWhile += step) <= endOfTheRangeWithStepUsingWhile) {
+        std::cout << beginningOfTheRangeWithStepUsingWhile << std::endl;
+    }
+    return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printAllNumbersInRangeWithStepUsingWhile();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
